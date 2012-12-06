1a24073192ecb00d9ad69692ac9d75b4367c5fe2
dicarlo2 <alexdicarlo@gmail.com>
2012-12-06 00:44:23 -0600

    Ch.6 Added backtracking search

--- aima-core/src/main/scala/aima/core/search/csp/Variable.scala
+++ aima-core/src/main/scala/aima/core/search/csp/Variable.scala
@@ -1,3 +1,10 @@
 package aima.core.search.csp
 
-case class Variable[A](domain: Set[A])
+case class Variable[A](num: Int, domain: Set[A]) {
+  override def equals(other: Any): Boolean = other match {
+    case that: Variable[A] => (that canEqual this) && that.num == this.num
+    case _ => false
+  }
+
+  override def hashCode(): Int = num.hashCode()
+}
--- aima-core/src/main/scala/aima/core/search/csp/backtrackingSearch.scala
+++ aima-core/src/main/scala/aima/core/search/csp/backtrackingSearch.scala
@@ -1,5 +1,29 @@
 package aima.core.search.csp
 
-class backtrackingSearch {
+object backtrackingSearch {
+  type SelectUnassignedVariable = CSP => Variable[_]
+  type OrderDomainValues = (Variable[_], CSP) => Set[_]
+  type Inference = CSP => Option[CSP]
 
+  def apply(select: SelectUnassignedVariable, order: OrderDomainValues, infer: Inference)(csp: CSP): Option[CSP] = {
+    def backtrack(csp: CSP): Option[CSP] = {
+      if (csp.constraintsSatisfied()) return Some(csp)
+      val variable = select(csp)
+      def recur(variable: Variable[_], domain: List[_]): Option[CSP] = domain match {
+        case value :: tail =>
+          infer(csp.reduceDomain(variable, value)) match {
+            case Some(inferredCSP) =>
+              backtrack(inferredCSP) match {
+                case result@Some(_) => result
+                case _ => recur(variable, tail)
+              }
+            case None =>
+              recur(variable, tail)
+          }
+        case Nil => None
+      }
+      recur(variable, order(variable, csp).to[List])
+    }
+    backtrack(csp)
+  }
 }
