0590fb4b030ecf0316470b24fa01f43a453190af
dicarlo2 <alexdicarlo@gmail.com>
2012-12-12 10:31:00 -0600

    Ch.7 Added walkSAT

---
+++ aima-core/src/main/scala/aima/core/logic/propositional/walkSAT.scala
@@ -0,0 +1,24 @@
+package aima.core.logic.propositional
+
+import util.Random._
+import annotation.tailrec
+
+object walkSAT {
+  def apply(clauses: Set[Clause], p: Double, maxFlips: Int): Option[Model] = {
+    @tailrec
+    def recur(model: Model, iter: Seq[Int]): Option[Model] = {
+      if (clauses forall {_ isTrueIn model}) Some(model) else iter match {
+        case _ :: tail =>
+          val symbols = shuffle(clauses filterNot {_ isTrueIn model}).head.literals flatMap {_.symbols}
+          val flippedSymbol = if (nextDouble() < p)
+                                shuffle(symbols).head
+                              else symbols maxBy
+                                {symbol => clauses count {_ isTrueIn model + (symbol → !model.get(symbol).get)}}
+          recur(model + (flippedSymbol → model.get(flippedSymbol).get), tail)
+        case Nil => None
+      }
+    }
+    val model = (clauses flatMap {_.symbols} map {(_, if (nextDouble() < 0.5) true else false)}).toMap
+    recur(model, 1 to maxFlips)
+  }
+}
